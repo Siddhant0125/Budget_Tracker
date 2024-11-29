56f63e173e3b96a85081b1623e44b9f6a20f8724
Siddhant0125 <68683013+Siddhant0125@users.noreply.github.com>
2024-11-29 01:29:39 -0500

    Update budget.py

--- budget.py
+++ budget.py
@@ -1,10 +1,5 @@
 import streamlit as st
-import datetime
-import json
-from collections import defaultdict
 from groq import Groq
-import pandas as pd
-import plotly.express as px
 
 # Fetch the API key from Streamlit Secrets
 api_key = st.secrets["GROQ_API_KEY"]
